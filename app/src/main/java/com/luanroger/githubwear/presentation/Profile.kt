76be52aa0b946dde6ce5215e31bc8952cb092cd3
ROG <luan.roger.2003@gmail.com>
2025-01-08 19:14:46 -0300

    feat: add profile photo on page

--- app/src/main/java/com/luanroger/githubwear/presentation/Profile.kt
+++ app/src/main/java/com/luanroger/githubwear/presentation/Profile.kt
@@ -1,19 +1,35 @@
 package com.luanroger.githubwear.presentation
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.ScrollableDefaults
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.wear.compose.foundation.rememberActiveFocusRequester
+import androidx.wear.compose.foundation.rotary.RotaryScrollableDefaults.behavior
+import androidx.wear.compose.foundation.rotary.rotaryScrollable
+import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.google.android.horologist.compose.layout.ScreenScaffold
+import com.google.android.horologist.compose.layout.fillMaxRectangle
 import com.luanroger.githubwear.presentation.services.GitHubApi
+import com.luanroger.githubwear.presentation.services.User
 import kotlinx.coroutines.launch
 
 sealed interface ProfilePageState {
     object Loading: ProfilePageState
-    data class Success(val user: String): ProfilePageState
+    data class Success(val user: User): ProfilePageState
     data class Error(val message: String): ProfilePageState
 }
 
@@ -39,22 +55,45 @@ class ProfilePageViewModel: ViewModel() {
     }
 }
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun ProfilePage(uiState: ProfilePageState) {
-    when(uiState) {
-        is ProfilePageState.Loading -> {
-            Column {
-                Text("Loading...")
-            }
-        }
-        is ProfilePageState.Success -> {
-            Column {
-                Text("User: ${uiState.user}")
-            }
-        }
-        is ProfilePageState.Error -> {
-            Column {
-                Text("Error: ${uiState.message}")
+    val focusRequester = rememberActiveFocusRequester()
+    val scrollState = rememberScrollState()
+
+    ScreenScaffold(scrollState = scrollState) {
+        Column(
+            modifier =
+            Modifier.verticalScroll(scrollState)
+                .rotaryScrollable(
+                    behavior(
+                        scrollableState = scrollState,
+                        flingBehavior = ScrollableDefaults.flingBehavior()
+                    ),
+                    focusRequester = focusRequester
+                )
+        ) {
+            when(uiState) {
+                ProfilePageState.Loading -> {
+                    Text("Loading...")
+                }
+                is ProfilePageState.Success -> {
+                    GlideImage(
+                        model = uiState.user.avatar_url,
+                        contentDescription = "Descriptiom",
+                        modifier = Modifier.fillMaxRectangle(),
+                    )
+                    Text(uiState.user.login)
+                    Text(uiState.user.login)
+                    Text(uiState.user.login)
+                    Text(uiState.user.login)
+                    Text(uiState.user.login)
+                    Text(uiState.user.login)
+
+                }
+                is ProfilePageState.Error -> {
+                    Text((uiState).message)
+                }
             }
         }
     }
--- app/src/main/java/com/luanroger/githubwear/presentation/services/GitHubService.kt
+++ app/src/main/java/com/luanroger/githubwear/presentation/services/GitHubService.kt
@@ -1,21 +1,28 @@
 package com.luanroger.githubwear.presentation.services
 
-import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 private val BASE_URL = "https://api.github.com/"
 
+data class User(
+    val id: String,
+
+    val login: String,
+
+    val avatar_url: String
+)
+
 interface GitHubService {
     @GET("users/{user}")
-    suspend fun getUserInfo(@Path("user") user: String): String
+    suspend fun getUserInfo(@Path("user") user: String): User
 }
 
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(BASE_URL)
+    .addConverterFactory(GsonConverterFactory.create())
     .build()
 
 object GitHubApi {
